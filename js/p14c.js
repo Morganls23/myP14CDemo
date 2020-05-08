d1c0e648610312549c99d08ae585c23393b534a3
Morgan <morgan.smith118@gmail.com>
2020-05-08 00:18:21 -0600

    Update p14c.js

--- js/p14c.js
+++ js/p14c.js
@@ -87,9 +87,10 @@ function exJax(method, url, callback, contenttype, payload) {
 // change password function
 function changePassword() {
   console.log('changePassword called');
+  let newpass = $('new_password').val();
   let payload = JSON.stringify({
     currentPassword: $('#current_password').val(),
-    newPassword: $('#new_password').val()
+    newPassword: newpass
   });
   let url = $('#changePasswordUrl').val();
   let contenttype = 'application/vnd.pingidentity.password.reset+json';
